48f2fd7ca7034c6ae700f0776be9ef451466bf00
kenji <hotdrop.tkenji@gmail.com>
2022-06-11 11:52:54 +0900

    fix: お薬一覧が画面一杯登録されている時、fabが邪魔で編集ボタンが押せなかったので修正

--- lib/ui/medicine/medicine_page.dart
+++ lib/ui/medicine/medicine_page.dart
@@ -7,6 +7,7 @@ import 'package:dyphic/ui/medicine/medicine_view_model.dart';
 import 'package:dyphic/ui/widget/app_dialog.dart';
 import 'package:dyphic/ui/widget/app_progress_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MedicinePage extends ConsumerWidget {
@@ -23,17 +24,25 @@ class MedicinePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final uiState = ref.watch(medicineViewModelProvider).uiState;
     return uiState.when(
-      loading: (err) => _onLoading(context, err),
-      success: () => _onSuccess(context, ref),
+      loading: (err) => _ViewLoading(errorMessage: err),
+      success: () => const _ViewSuccess(),
     );
   }
+}
 
-  Widget _onLoading(BuildContext context, String? errorMsg) {
+class _ViewLoading extends StatelessWidget {
+  const _ViewLoading({Key? key, this.errorMessage}) : super(key: key);
+
+  final String? errorMessage;
+
+  @override
+  Widget build(BuildContext context) {
     Future.delayed(Duration.zero).then((_) async {
-      if (errorMsg != null) {
-        await AppDialog.onlyOk(message: errorMsg).show(context);
+      if (errorMessage != null) {
+        await AppDialog.onlyOk(message: errorMessage!).show(context);
       }
     });
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(AppStrings.medicinePageTitle),
@@ -43,33 +52,101 @@ class MedicinePage extends ConsumerWidget {
       ),
     );
   }
+}
 
-  Widget _onSuccess(BuildContext context, WidgetRef ref) {
+class _ViewSuccess extends ConsumerStatefulWidget {
+  const _ViewSuccess({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => __ViewSuccessState();
+}
+
+class __ViewSuccessState extends ConsumerState<_ViewSuccess> {
+  bool _visibleFab = true;
+
+  @override
+  Widget build(BuildContext context) {
     final isSigniIn = ref.watch(appSettingsProvider).isSignIn;
     return Scaffold(
       appBar: AppBar(
         title: const Text(AppStrings.medicinePageTitle),
-        actions: [
-          IconButton(
-            onPressed: () async => await _showRefreshDialog(context, ref),
-            icon: const Icon(Icons.refresh),
-          ),
-        ],
+        actions: const [_RefreshIcon()],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(bottom: 32.0),
-        child: _viewContents(context, ref),
+      body: NotificationListener<UserScrollNotification>(
+        onNotification: ((notification) {
+          if (notification.direction == ScrollDirection.forward) {
+            setState(() => _visibleFab = true);
+          } else if (notification.direction == ScrollDirection.reverse) {
+            setState(() => _visibleFab = false);
+          }
+          return true;
+        }),
+        child: const Padding(
+          padding: EdgeInsets.only(bottom: 32.0),
+          child: _ViewContents(),
+        ),
       ),
       floatingActionButton: isSigniIn
-          ? FloatingActionButton(
-              onPressed: () async => await _processAdd(context, ref),
-              child: const Icon(Icons.add),
+          ? Visibility(
+              visible: _visibleFab,
+              child: const _MedicineAddFab(),
             )
           : null,
     );
   }
+}
 
-  Widget _viewContents(BuildContext context, WidgetRef ref) {
+class _RefreshIcon extends ConsumerWidget {
+  const _RefreshIcon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return IconButton(
+      onPressed: () async => await _showRefreshDialog(context, ref),
+      icon: const Icon(Icons.refresh),
+    );
+  }
+
+  Future<void> _showRefreshDialog(BuildContext context, WidgetRef ref) async {
+    AppDialog.okAndCancel(
+      message: AppStrings.medicineRefreshConfirmMessage,
+      onOk: () async => await _refresh(context, ref),
+    ).show(context);
+  }
+
+  Future<void> _refresh(BuildContext context, WidgetRef ref) async {
+    const progressDialog = AppProgressDialog<void>();
+    await progressDialog.show(
+      context,
+      execute: ref.read(medicineViewModelProvider).refresh,
+      onSuccess: (_) => {/* 成功時は何もしない */},
+      onError: (err) => AppDialog.onlyOk(message: err).show(context),
+    );
+  }
+}
+
+class _MedicineAddFab extends ConsumerWidget {
+  const _MedicineAddFab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return FloatingActionButton(
+      onPressed: () async => await _processAdd(context, ref),
+      child: const Icon(Icons.add),
+    );
+  }
+
+  Future<void> _processAdd(BuildContext context, WidgetRef ref) async {
+    final newEmptyMeidine = ref.read(medicineProvider.notifier).newMedicine();
+    await MedicineEditPage.start(context, newEmptyMeidine);
+  }
+}
+
+class _ViewContents extends ConsumerWidget {
+  const _ViewContents({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     final medicines = ref.watch(medicineProvider);
     if (medicines.isEmpty) {
       return const Center(
@@ -90,27 +167,5 @@ class MedicinePage extends ConsumerWidget {
         );
       },
     );
-  }
-
-  Future<void> _showRefreshDialog(BuildContext context, WidgetRef ref) async {
-    AppDialog.okAndCancel(
-      message: AppStrings.medicineRefreshConfirmMessage,
-      onOk: () async => await _refresh(context, ref),
-    ).show(context);
-  }
-
-  Future<void> _refresh(BuildContext context, WidgetRef ref) async {
-    const progressDialog = AppProgressDialog<void>();
-    await progressDialog.show(
-      context,
-      execute: ref.read(medicineViewModelProvider).refresh,
-      onSuccess: (_) => {/* 成功時は何もしない */},
-      onError: (err) => AppDialog.onlyOk(message: err).show(context),
-    );
-  }
-
-  Future<void> _processAdd(BuildContext context, WidgetRef ref) async {
-    final newEmptyMeidine = ref.read(medicineProvider.notifier).newMedicine();
-    await MedicineEditPage.start(context, newEmptyMeidine);
   }
 }
